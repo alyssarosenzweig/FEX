9c46200fb1861dcdbaffed295b8435ba9e2abddd
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2024-05-02 14:49:18 -0400

    .
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- toolchain_x86_32.cmake
+++ toolchain_x86_32.cmake
@@ -1,6 +1,6 @@
 option(ENABLE_CLANG_THUNKS "Enable building thunks with clang" FALSE)
 
-set(CMAKE_SYSTEM_PROCESSOR i686)
+set(CMAKE_SYSTEM_PROCESSOR i386)
 
 if (ENABLE_CLANG_THUNKS)
   message(STATUS "Enabling thunk clang building. Force enabling LLD as well")
@@ -10,7 +10,7 @@ if (ENABLE_CLANG_THUNKS)
   set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
   set(CMAKE_C_COMPILER zig cc)
   set(CMAKE_CXX_COMPILER zig c++)
-  set(CLANG_FLAGS "-target i686-linux-gnu -msse2 -mfpmath=sse")
+  set(CLANG_FLAGS "-target i386-linux-gnu -msse2 -mfpmath=sse")
 
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CLANG_FLAGS}")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CLANG_FLAGS}")
